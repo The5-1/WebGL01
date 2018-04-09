b38355286184d7cbf4cc29f22c351c2a4a932bc1
The5-1 <agaku@hotmail.de>
2018-04-09 19:28:42 +0200

    typescript sample

--- build/TypeScript01.js
+++ build/TypeScript01.js
@@ -2,9 +2,26 @@
 //1.) Download VS2017 package and install https://www.typescriptlang.org/#download-links
 //2.) add C:\Program Files (x86)\Microsoft SDKs\TypeScript\2.8 to PATH so it finds tsc.exe
 //3.) https://code.visualstudio.com/docs/languages/typescript
-function greeter(person) {
+//4.) https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+//5.) [Ctrl]+[Shift]+[B] to build (remapped to [F5])
+function greeter1(person) {
     return "Hello, " + person;
 }
-var user = "User";
-var test = 2;
-document.body.innerHTML = greeter(user);
+var user1 = "User"; //works!
+//let user = [0,1,2];   // Argument of type 'number[]' is not assignable to parameter of type 'string'.
+console.log(greeter1(user1));
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("James", "M.", "Willson");
+console.log(greeter(user));
+console.log(user.fullName);
--- ts/TypeScript01.ts
+++ ts/TypeScript01.ts
@@ -2,14 +2,37 @@
 //1.) Download VS2017 package and install https://www.typescriptlang.org/#download-links
 //2.) add C:\Program Files (x86)\Microsoft SDKs\TypeScript\2.8 to PATH so it finds tsc.exe
 //3.) https://code.visualstudio.com/docs/languages/typescript
+//4.) https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+//5.) [Ctrl]+[Shift]+[B] to build (remapped to [F5])
 
 
-function greeter(person) {
+function greeter1(person: string) //": string" annotation
+{
     return "Hello, " + person;
 }
 
-let user = "User";
-let test = 2;
+let user1 = "User";      //works!
+//let user = [0,1,2];   // Argument of type 'number[]' is not assignable to parameter of type 'string'.
 
-document.body.innerHTML = greeter(user);
+console.log(greeter1(user1));
 
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("James", "M.", "Willson");
+
+console.log(user.fullName);
+console.log(greeter(user));
